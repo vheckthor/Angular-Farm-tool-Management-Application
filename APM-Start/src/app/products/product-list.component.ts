ee73f8860aa31f615906258e6bab831861ba0fd0
Dare Victor Adebayo <adebayovicktor@gmail.com>
2020-04-06 14:03:14 +0100

    route configuration done

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -3,7 +3,6 @@
 import { ProductService } from './product-services';
 
  @Component({
-     selector:'pm-products',
      templateUrl:'./product-list.component.html',
      styleUrls:['./product-list.component.css']
  })
